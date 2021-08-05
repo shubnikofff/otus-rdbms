08b7cb89c16aeb796f91c2288608717207a7fb7f
Alexey Shubnikov <alexey.shubnikov@paxful.com>
2021-08-05 20:26:09 +0300

    Task-05: Created queries using LEFT JOIN and INNER JOIN

--- task-05/dml.sql
+++ task-05/dml.sql
@@ -31,3 +31,12 @@ values (1, 3),
 returning
     (select name from catalog.products where id = product_id) as product,
     (select name from catalog.product_categories where id = products_product_categories.category_id) as category;
+
+-- Пример запроса с использованием LEFT JOIN. Результат: найдены все товары и их категории, если такие имеются. Если поменять порядок соединений в FROM, то будут найдены только те товары, которые принаддежат хотя бы к одной категории.
+-- Это происходит потому, что в результат поиска попадают все строки из левой таблицы и соответсвующие усдловию в ON строки из правой.
+-- Соответственно товары, для которых не было найдено категории в результат не попадут.
+select p.name as product, (select name from catalog.product_categories pc where pc.id = ppc.category_id) as category from catalog.products p left join catalog.products_product_categories ppc on p.id = ppc.product_id;
+
+-- Пример запроса с INNER JOIN. Результат: найдены только те товары, которые принаддежат хотя бы к одной категории.
+-- Изменение порядка соединений не дает никакого эффекта, так как при INNER JOIN в результат попадают только те строки, которые есть в обеих таблицах и соответствуют условаию ON.
+select p.name as product, (select name from catalog.product_categories pc where pc.id = ppc.category_id) as category from catalog.products p inner join catalog.products_product_categories ppc on p.id = ppc.product_id;
