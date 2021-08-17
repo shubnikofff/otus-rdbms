665eff8fee5d34ce92a6abec09b65ebea9b91fe3
Alexey Shubnikov <shubnikov.av@gmail.com>
2021-08-17 22:48:32 +0300

    Task-06: Added index on field producer_id

--- task-06/indexes.sql
+++ task-06/indexes.sql
@@ -1,16 +1,24 @@
-drop index catalog.products_name_idx cascade;
 -- 1) Создание индекса
 create index products_name_idx on catalog.products (name);
+create index products_producer_id_idx on catalog.products (producer_id);
 analyse catalog.products;
 
-select pg_size_pretty(pg_table_size('products')) as table_size,
-       pg_size_pretty(pg_table_size('products_name_idx')) as index_size;
+select pg_size_pretty(pg_table_size('products'))                 as table_size,
+       pg_size_pretty(pg_table_size('products_name_idx'))        as products_name_index_size,
+       pg_size_pretty(pg_table_size('products_producer_id_idx')) as producer_id_index_size;
 
-explain
-select name
+explain analyze
+select products.name, p.name
 from catalog.products
-where name = 'NIVEA Gift box classic';
+         left join producers p on p.id = products.producer_id
+where products.name = 'NIVEA Gift box classic';
 
 -- 2) Вывод команды explain:
--- Index Only Scan using products_name_idx on products  (cost=0.42..4.44 rows=1 width=26)
--- Index Cond: (name = 'NIVEA Gift box classic'::text)
+-- Nested Loop Left Join  (cost=0.72..16.75 rows=1 width=42) (actual time=0.016..0.017 rows=0 loops=1)
+--   ->  Index Scan using products_name_idx on products  (cost=0.42..8.44 rows=1 width=34) (actual time=0.016..0.016 rows=0 loops=1)
+--         Index Cond: ((name)::text = 'NIVEA Gift box classic'::text)
+--   ->  Index Scan using producers_pkey on producers p  (cost=0.29..8.31 rows=1 width=24) (never executed)
+--         Index Cond: (id = products.producer_id)
+-- Planning Time: 0.180 ms
+-- Execution Time: 0.036 ms
+
