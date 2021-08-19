1d72f0cb4d6f4921a83dda9f92e3159f05f3b9da
Alexey Shubnikov <alexey.shubnikov@paxful.com>
2021-08-19 14:51:10 +0300

    Task-07: Created request for the sum of game points with grouping and sorting by years

--- task-07/count.sql
+++ task-07/count.sql
@@ -23,3 +23,5 @@ values ('Mike', 1, 2018, 18),
        ('Jack', 3, 2020, 18),
        ('Jackie', 4, 2020, 29),
        ('Jet', 5, 2020, 27);
+
+select year_game, count(points) from statistic group by year_game order by year_game;
