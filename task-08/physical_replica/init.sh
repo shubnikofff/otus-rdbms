9014fd946b479437997f13720d215e11ced216ce
Alexey Shubnikov <shubnikov.av@gmail.com>
2021-09-25 19:40:28 +0300

    Task-08: Added creation replication slot

--- task-08/physical_replica/init.sh
+++ task-08/physical_replica/init.sh
@@ -2,4 +2,4 @@
 set -e
 
 rm -rf /var/lib/postgresql/data/*
-pg_basebackup -p 5432 -h master -R -D /var/lib/postgresql/data/ -U postgres -W
+pg_basebackup -p 5432 -h master -R -D /var/lib/postgresql/data/ -U postgres -W -C --slot=otus
